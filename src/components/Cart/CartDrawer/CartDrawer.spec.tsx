e42d6dc3722edd5fcdf20b4b1af856209b94125a
unknown <kieran6roberts@gmail.com>
2021-01-30 15:52:31 +0100

    add CartDrawer test for empty cart

--- src/components/Cart/CartDrawer/CartDrawer.spec.tsx
+++ src/components/Cart/CartDrawer/CartDrawer.spec.tsx
@@ -4,12 +4,26 @@ import * as React from "react";
 import CartDrawer from "@/components/Cart/CartDrawer/CartDrawer";
 
 import { render } from "../../../../test-utils";
+import userEvent from "@testing-library/user-event";
 
 describe("<CartDrawer />", () => {
 
-    test("renders", () => {
+    test("renders in close position", () => {
         render(<CartDrawer />, null);
         
-        expect(screen.getByText("No items in cart")).toBeInTheDocument();
+        expect(screen.getByRole("button")).toHaveTextContent(/cart/i);
+        expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+    });
+
+    test("opens with empty cart", () => {
+        render(<CartDrawer />, null);
+
+        userEvent.click(screen.getByRole("button"));
+
+        expect(screen.getByRole("dialog")).toBeInTheDocument();
+        expect(screen.getByRole("dialog", { name: "What's in your bag" })).toBeInTheDocument();
+        expect(screen.getAllByRole("list")[6]).toContainElement(screen.getByText(/no items in cart/i));
+        expect(screen.getByRole("contentinfo")).toHaveTextContent("Total: £0");
+        expect(screen.getByRole("button", { name: "Close" })).toBeInTheDocument();
     });
 });
