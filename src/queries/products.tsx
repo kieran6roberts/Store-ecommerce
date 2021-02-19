21a2582b893e1e740214faa2a005bd61e008ce57
unknown <kieran6roberts@gmail.com>
2021-02-19 12:23:06 +0100

    added product data fragment and refactored other queries

--- src/queries/products.tsx
+++ src/queries/products.tsx
@@ -1,84 +1,70 @@
 import { gql } from "@apollo/client";
 
+export const PRODUCT_DATA = gql`
+    fragment ProductParts on Product {
+        category {
+            name
+        }
+        description {
+            text
+        }
+        id
+        name
+        price
+        __typename
+    }
+`;
+
 export const PRODUCT_NAMES = gql`
     query GetProductTitles {
         products {
             name
         }
     }
+    ${PRODUCT_DATA}
 `;
 
 export const PRODUCT_INFO = gql`
     query GetSingleProduct($name: String!) {
         products(where: {name: $name}) {
-            category {
-                name
-            }
-            description {
-                text
-            }
-            id
-            name
-            price
+            ...ProductParts
         }
     }
+    ${PRODUCT_DATA}
 `;
 
 export const PRODUCT_NEW = gql`
     query GetNewProducts {
         products(first: 3, orderBy: createdAt_DESC) {
-            category {
-                name
-            }
-            description {
-                text
-            }
-            id
-            name
-            price
+           ...ProductParts
+        }
     }
-}`;
+    ${PRODUCT_DATA}
+`;
 
 export const PRODUCT_ALL = gql`
     query GetAllProducts($offset: Int, $limit: Int) {
         products(first: $limit, skip: $offset ) {
-            category {
-                name
-            }
-            description {
-                text
-            }
-            id
-            name
-            price
+           ...ProductParts
         }
     }
+    ${PRODUCT_DATA}
 `;
 
 export const PRODUCT_STORAGE = gql`
     query GetStorageProducts($ids: [ID!]!) {
         products(where: { id_in: $ids }) {
-            ... on Product {
-                category {
-                    name
-                }
-                description {
-                    text
-                }
-                id
-                name
-                price
-            }
+            ...ProductParts
         }
     }
+    ${PRODUCT_DATA}
 `;
 
 export const PRODUCT_SORT = gql`
-    query ProductSort($sort: ProductOrderByInput!, $price: Int) {
-        products(orderBy: price_ASC, where: { price: $price }) {
-            id
-            name
-            price
+    query ProductSort($sort: ProductOrderByInput!) {
+        products(orderBy: price_ASC) {
+            ...ProductParts
         }
     }
+    ${PRODUCT_DATA}
 `;
