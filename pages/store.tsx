adfd4d116aba894dc6315b1bee5307610dc3108e
unknown <kieran6roberts@gmail.com>
2021-02-20 13:21:37 +0100

    add fallback for no products matching filter

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,5 +1,9 @@
 import { useLazyQuery, useQuery } from "@apollo/client";
-import { Flex } from "@chakra-ui/react";
+import { 
+  Box,
+  Flex, 
+  Heading, 
+  Text } from "@chakra-ui/react";
 import { NextPage } from "next";
 import * as React from "react";
 
@@ -15,24 +19,18 @@ import { GET_CATEGORY,
  
 const Store: NextPage = () => {
 
-  const [ sortProducts, setSortProducts ] = React.useState<{ products: IProductQuery[] } | []>([]);
+  const [ sortProducts, setSortProducts ] = React.useState<{ products: IProductQuery[] | [] } | []>([]);
 
   const { data: categories, error, loading: loadingCategories } = useQuery(PRODUCT_CATEGORIES);
 
   const [ handleAscPrice, { loading: sortError }] = useLazyQuery(PRODUCT_SORT, {
     fetchPolicy: "no-cache",
-    onCompleted: data => {
-      console.log(data);
-      setSortProducts(data);
-    }
+    onCompleted: data => setSortProducts(data)
   });
   
   const [ handleCategoryFilter, { loading: filterLoading }] = useLazyQuery(GET_CATEGORY, {
     fetchPolicy: "no-cache",
-    onCompleted: data => {
-      console.log(data);
-      setSortProducts(data);
-    }
+    onCompleted: data => setSortProducts(data)
   });
 
   if (loadingCategories) {
@@ -41,6 +39,26 @@ const Store: NextPage = () => {
 
   return (
     <Layout>
+      <Box 
+      as="header"
+      mb={8}
+      textAlign="center"
+      >
+        <Heading 
+        as="h1"
+        mb={6}
+        >
+          Welcome to our store!
+        </Heading>
+        <Text 
+        maxW="800px"
+        mx="auto"
+        >
+          Our focus is to bring you the very best in the world of coffee. Everything from
+          fresh coffee beans, the best coffee powders and capsules as well as other miscellaneous
+          items such as cups and mugs. Take a look to see if anything takes your fancy.
+        </Text>
+      </Box>
       <Flex 
       justify="center"
       mb={12}
@@ -51,6 +69,14 @@ const Store: NextPage = () => {
         handleCategoryFilter={handleCategoryFilter} 
         />
       </Flex>
+      {sortProducts.products.length === 0 ? 
+      <Text 
+      color="pink.100"
+      textAlign="center"
+      >
+        No Products Matching this search
+      </Text>
+      :
       <Products 
       sortProducts={sortProducts}
       loadMore={true} 
@@ -64,6 +90,7 @@ const Store: NextPage = () => {
         ssr: false
       }}
       />
+    }
     </Layout>
   );
 };
